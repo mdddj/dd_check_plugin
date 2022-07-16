b30ac2885ef389f2bdcf07002d1a099972fb6df6
梁典典 <413153189@qq.com>
2022-07-16 15:56:53 +0800

    属性优化

--- lib/dd_check_plugin.dart
+++ lib/dd_check_plugin.dart
@@ -13,7 +13,12 @@ class DdCheckPlugin {
 
   static DdCheckPlugin get instance => DdCheckPlugin._();
 
-  //初始化
+  ///初始化
+  ///[defaultProjectName] - 自定义项目命名
+  ///[port] - 指定连接端口
+  ///[hostHandle] - ip过滤,会回调一个尝试连接的host ip地址,如果返回host,将不会尝试连接到该地址
+  ///[timeOut] - 连接超时时间,建议超过800毫秒
+  ///[initHost] - 指定连接IP,可以加快连接速度
   Future<void> init(Dio dio, {String? defaultProjectName, int? port, HostHandle? hostHandle, Duration? timeOut, String? initHost}) async {
     await _connect(defaultProjectName, port: port, hostHandle: hostHandle, timeOut: timeOut, initHost: initHost);
     addInterceptors(dio);
