42045450cdc7c3d82b20b8254bbb5f880e588320
梁典典 <413153189@qq.com>
2022-08-15 09:11:39 +0800

    新增异常处理

--- lib/ip_util.dart
+++ lib/ip_util.dart
@@ -2,7 +2,15 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:network_info_plus/network_info_plus.dart';
+class ConnectException implements Exception{
+  final String msg;
+  const ConnectException(this.msg):super();
+  @override
+  String toString() {
+    return msg;
+  }
 
+}
 typedef HostHandle = bool Function(String host);
 
 ///扫描端口失败异常
@@ -30,37 +38,42 @@ class IpUtil {
 
   /// 获取服务器IP,也就是用户电脑的IP
   Future<String> checkConnectServerAddress(int serverPort, {ValueChanged<Socket>? conectSuccess, HostHandle? hostHandle, Duration? timeOut,String? initHost}) async {
-    List<Future<String>> futureList = [];
-    if(initHost!=null){
-      var s = await Socket.connect(initHost, serverPort, timeout: timeOut);
-      conectSuccess?.call(s);
-      return initHost;
-    }else{
-      String? ip = await NetworkInfo().getWifiIP();
-      if (ip != null) {
-        final indexs = List.generate(256, (index) => index + 1);
-        await Future.forEach(indexs, (element) async {
-          var v = "";
-          if (v.isEmpty) {
-            v = await Future<String>.sync(() async {
-              final host = '${ip.substring(0, ip.lastIndexOf('.'))}.$element';
-              final isHandle = hostHandle?.call(host) ?? true;
-              if (isHandle) {
-                try {
-                  var s = await Socket.connect(host, serverPort, timeout: timeOut);
-                  conectSuccess?.call(s);
-                  return host;
-                } catch (e) {
-                  return '';
+
+    try{
+      List<Future<String>> futureList = [];
+      if(initHost!=null){
+        var s = await Socket.connect(initHost, serverPort, timeout: timeOut);
+        conectSuccess?.call(s);
+        return initHost;
+      }else{
+        String? ip = await NetworkInfo().getWifiIP();
+        if (ip != null) {
+          final indexs = List.generate(256, (index) => index + 1);
+          await Future.forEach(indexs, (element) async {
+            var v = "";
+            if (v.isEmpty) {
+              v = await Future<String>.sync(() async {
+                final host = '${ip.substring(0, ip.lastIndexOf('.'))}.$element';
+                final isHandle = hostHandle?.call(host) ?? true;
+                if (isHandle) {
+                  try {
+                    var s = await Socket.connect(host, serverPort, timeout: timeOut);
+                    conectSuccess?.call(s);
+                    return host;
+                  } catch (e) {
+                    return '';
+                  }
                 }
-              }
-              return "";
-            });
-          }
-        });
+                return "";
+              });
+            }
+          });
+        }
+        List<String> results = await Future.wait<String>(futureList);
+        return results.firstWhere((e) => e.isNotEmpty, orElse: () => '');
       }
-      List<String> results = await Future.wait<String>(futureList);
-      return results.firstWhere((e) => e.isNotEmpty, orElse: () => '');
+    }catch(e,s){
+      throw ConnectException("连接IDEA-Dio Request模块失败:\n$e\n$s");
     }
 
   }
